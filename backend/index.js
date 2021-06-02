be6d61861b6200cbd87ad134e92ace65060b6fec
Luis Aparicio <luisaparicio@ufl.edu>
2021-06-01 20:51:56 -0400

    add gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const config = require("./config/db")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const cors = require('cors')
